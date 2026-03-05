995fa1527ad61073a6d22aeb73d1b97977ceeef9
Jonatam R. <jonatamsr@gmail.com>
2026-03-04 21:25:52 -0300

    refactor: update timezone setting to America/Sao_Paulo in bootstrap file

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -17,7 +17,7 @@ ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
 error_reporting(E_ALL);
-date_default_timezone_set('Asia/Shanghai');
+date_default_timezone_set('America/Sao_Paulo');
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 
