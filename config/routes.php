cbf8b5e0d2205429863ac0231c75e985e3a5ce07
Jonatam R. <jonatamsr@gmail.com>
2026-03-06 07:41:37 -0300

    refactor: Remove unused files and clean up project structure

--- config/routes.php
+++ config/routes.php
@@ -8,14 +8,8 @@ use App\Middleware\IdempotencyMiddleware;
 use App\Middleware\RateLimitMiddleware;
 use Hyperf\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
-
 Router::get('/health', [HealthController::class, 'check']);
 
 Router::post('/account/{accountId}/balance/withdraw', [AccountWithdrawController::class, 'withdraw'], [
     'middleware' => [IdempotencyMiddleware::class, RateLimitMiddleware::class],
 ]);
-
-Router::get('/favicon.ico', function () {
-    return '';
-});
