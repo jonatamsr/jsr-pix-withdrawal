ee46f3f032161d7ec9e68178015f308fdc5087bf
Jonatam R. <jonatamsr@gmail.com>
2026-03-06 03:14:05 -0300

    feat: Add ProcessScheduledWithdrawsCrontab and corresponding unit tests

---
+++ app/Crontab/ProcessScheduledWithdrawsCrontab.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Crontab;
+
+use App\Application\UseCase\ProcessScheduledWithdrawsUseCase;
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Crontab\Annotation\Crontab;
+use Throwable;
+
+#[Crontab(
+    name: 'process-scheduled-withdraws',
+    rule: '* * * * *',
+    memo: 'Process scheduled withdrawals every minute',
+    onOneServer: true,
+)]
+class ProcessScheduledWithdrawsCrontab
+{
+    public function __construct(
+        private readonly ProcessScheduledWithdrawsUseCase $useCase,
+        private readonly StdoutLoggerInterface $logger,
+    ) {}
+
+    public function execute(): void
+    {
+        try {
+            $this->logger->info('Crontab [process-scheduled-withdraws] started.');
+
+            $this->useCase->execute();
+
+            $this->logger->info('Crontab [process-scheduled-withdraws] finished.');
+        } catch (Throwable $e) {
+            $this->logger->error('Crontab [process-scheduled-withdraws] failed: ' . $e->getMessage(), [
+                'exception' => $e::class,
+                'trace' => $e->getTraceAsString(),
+            ]);
+        }
+    }
+}
