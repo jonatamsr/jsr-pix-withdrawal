e2e88e37fbf73fb000ec5d657cb0d395120006e2
Jonatam R. <jonatamsr@gmail.com>
2026-03-05 23:34:19 -0300

    feat: add HyperfEventDispatcherAdapter and update dependencies configuration

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 use App\Domain\Port\AccountRepositoryInterface;
+use App\Domain\Port\EventDispatcherInterface;
 use App\Domain\Port\WithdrawRepositoryInterface;
+use App\Infrastructure\EventDispatcher\HyperfEventDispatcherAdapter;
 use App\Infrastructure\Persistence\Repository\EloquentAccountRepository;
 use App\Infrastructure\Persistence\Repository\EloquentWithdrawRepository;
 
 return [
     AccountRepositoryInterface::class => EloquentAccountRepository::class,
     WithdrawRepositoryInterface::class => EloquentWithdrawRepository::class,
+    EventDispatcherInterface::class => HyperfEventDispatcherAdapter::class,
 ];
